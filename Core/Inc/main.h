9404cb40ed02bb32bc1d755f910e6664c5e403e5
Pedro Galvão <90785456+Ordep-42@users.noreply.github.com>
2026-07-14 22:07:38 -0300

    update: Constante do timer no cubeMX.

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -60,7 +60,7 @@ void Error_Handler(void);
 #define SENSOR_I2C hi2c1
 #define GNSS_UART huart1
 #define DEBUG_UART huart3
-#define AHT10_TIM htim4
+#define I2C_SENSOR_TIMER htim4
 #define STATUS_LED_Pin GPIO_PIN_13
 #define STATUS_LED_GPIO_Port GPIOC
 
